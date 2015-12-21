c7900241f01ab20bbdcb3166c9b47ff1c118a2ce
Dan Cartier <nospamdan1@gmail.com>
2015-12-20 23:44:48 -0500

    msm8660: Fix audio makefile

--- legacy/Android.mk
+++ legacy/Android.mk
@@ -4,9 +4,7 @@ ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-include $(MY_LOCAL_PATH)/legacy/Android.mk
-endif
+include $(call all-subdir-makefiles)
 
 endif
 

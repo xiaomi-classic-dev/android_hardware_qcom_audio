60d49367a1c4d356bf297884f0862d59f66383d5
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-12-21 21:13:47 -0500

    use project path map

--- legacy/Android.mk
+++ legacy/Android.mk
@@ -1,11 +1,6 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
+AUDIO_HW_ROOT := $(call my-dir)
 
-ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
-
-MY_LOCAL_PATH := $(call my-dir)
-
-include $(call all-subdir-makefiles)
-
-endif
-
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+    include $(AUDIO_HW_ROOT)/msm8660/Android.mk
+    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
 endif

e97d94a1d0cc151b7432858fdc55a5a9825b3699
Ivan Grinko <iivanich@gmail.com>
2016-02-23 12:28:42 +0200

    audio/msm8x60: USE_LEGACY_AUDIO_POLICY

--- legacy/msm8660/Android.mk
+++ legacy/msm8660/Android.mk
@@ -82,6 +82,7 @@ LOCAL_CFLAGS += $(common_cflags) -Wno-error
 
 include $(BUILD_SHARED_LIBRARY)
 
+ifeq ($(USE_LEGACY_AUDIO_POLICY), 1)
 # The audio policy is implemented on top of legacy policy code
 include $(CLEAR_VARS)
 
@@ -111,6 +112,7 @@ LOCAL_C_INCLUDES += hardware/libhardware_legacy/audio
 LOCAL_CFLAGS += $(common_cflags) -Wno-error
 
 include $(BUILD_SHARED_LIBRARY)
+endif
 
 # Load audio_policy.conf to system/etc/
 include $(CLEAR_VARS)
